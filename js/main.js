69d4635113b3afc8836353fe5b1ef27d67393d14
Emilio Lopez <emilio.lopez@jivesoftware.com>
2015-02-09 01:28:19 -0800

    remove a couple of empty lines

--- js/main.js
+++ js/main.js
@@ -3,10 +3,8 @@
 */
 
 (function() {
-
     App.models.contacts = new App.Models.ContactCollection();
     var contactView = new App.Views.ContactView(App.models.contacts, "#contact-list");
     var actionView = new App.Views.ActionView(new App.Models.ActionCollection(), "#action-list");
     var jsonView = new App.Views.JsonView(null, "#json");
-
 }).call(this);
